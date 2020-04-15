04e2b6bfa9539fb54228eb3cb41d6ff5f5903711
Maxwell Hankner <maxwellhankner@gmail.com>
2020-04-15 14:32:27 -0500

    send user to google

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 // index route loads view.html
 router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  // res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile("http://www.google.com");
 });
 
 module.exports = router;
