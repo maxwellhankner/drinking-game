e98075a5648549d0e9317cb910b8818ff062f082
skvisual <southkvisual@gmail.com>
2020-04-27 17:21:08 -0500

    service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,10 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/favorites.html",
-    "/topic.html",
     "/assets/css/style.css",
-    "/dist/app.bundle.js",
-    "/dist/favorites.bundle.js",
-    "/dist/topic.bundle.js"
+    "/assets/js/audio.js",
+    "/assets/js/interface.js",
+    "/assets/js/socket.js",
 ];
 
 
